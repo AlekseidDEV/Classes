5b82a9dc1ce53dca54967c4672515bfda10a99c4
Lesha <lesha.rudiko@gmail.com>
2023-08-16 20:05:29 +0300

    форматнул документ, новый форматировщик идеально редачит код

--- js/script.js
+++ js/script.js
@@ -12,43 +12,43 @@ const tableUserBody = document.querySelector('.sub')
 
 let workerArr = []
 
-class Worker{
-    constructor(name, lastname,age, dataBirth){
+class Worker {
+    constructor(name, lastname, age, dataBirth) {
         this.userName = name
         this.useLastName = lastname
         this.useAge = age
         this.useDataBirth = dataBirth
     }
 
-    get userName(){
-        return this.name 
+    get userName() {
+        return this.name
     }
 
-    set userName(userName){
+    set userName(userName) {
         this.name = userName
     }
-     
 
-    get useAge(){
+
+    get useAge() {
         return this.age
     }
 
-    set useAge(userAge){
+    set useAge(userAge) {
         this.age = userAge
     }
 
-    get useDataBirth (){
+    get useDataBirth() {
         return this.dataBirth
     }
 
-    set useDataBirth(userBirth){
+    set useDataBirth(userBirth) {
         this.dataBirth = userBirth
     }
 
 }
 
-class FrontendDev extends Worker{
-    constructor(name, lastname, age, dataBirth, jobTitle){
+class FrontendDev extends Worker {
+    constructor(name, lastname, age, dataBirth, jobTitle) {
         super(name, lastname, age, dataBirth, jobTitle)
         this.userTitleJob = jobTitle
     }
@@ -56,27 +56,27 @@ class FrontendDev extends Worker{
     salary = '50.000 рублей'
     Office = '1 офис'
 
-    get userTitleJob(){
+    get userTitleJob() {
         return this.userTitleJob
     }
 
-    set userTitleJob(userTitleJob){
+    set userTitleJob(userTitleJob) {
         this.jobTitle = userTitleJob
     }
 }
 
-class backendDev extends Worker{
-    constructor(name, lastname, age, dataBirth){
+class backendDev extends Worker {
+    constructor(name, lastname, age, dataBirth) {
         super(name, lastname, age, dataBirth)
-        
+
     }
 
     salary = '70.000 рублей'
     Office = '3 офис'
 }
 
-class designer extends Worker{
-    constructor(name, lastname, age, dataBirth){
+class designer extends Worker {
+    constructor(name, lastname, age, dataBirth) {
         super(name, lastname, age, dataBirth)
 
     }
@@ -85,8 +85,8 @@ class designer extends Worker{
     Office = '7 офис'
 }
 
-class googleAnalitick extends Worker{
-    constructor(name, lastname, age, dataBirth){
+class googleAnalitick extends Worker {
+    constructor(name, lastname, age, dataBirth) {
         super(name, lastname, age, dataBirth)
 
     }
@@ -113,33 +113,33 @@ const renderWorkerTable = () => {
         newField.classList.add('field_worker')
         tableUserBody.append(newField)
 
-        newField.innerHTML =    '<td>' + 
-                                elem.name +
-                                '</td>' +
-                                '<td>' + 
-                                elem.useLastName +
-                                '</td>' +
-                                '<td>' + 
-                                elem.age +
-                                '</td>' +
-                                '<td>' + 
-                                elem.dataBirth +
-                                '</td>' +
-                                '<td>' + 
-                                elem.salary +
-                                '</td>' +
-                                '<td>' + 
-                                elem.Office +
-                                '</td>' + 
-                                '<td>' + 
-                                elem.jobTitle +
-                                '</td>' + 
-                                '<td>' + 
-                                '<button class = "delete_user"> Удалить сотрудника</button>' +
-                                '</td>'
+        newField.innerHTML = '<td>' +
+            elem.name +
+            '</td>' +
+            '<td>' +
+            elem.useLastName +
+            '</td>' +
+            '<td>' +
+            elem.age +
+            '</td>' +
+            '<td>' +
+            elem.dataBirth +
+            '</td>' +
+            '<td>' +
+            elem.salary +
+            '</td>' +
+            '<td>' +
+            elem.Office +
+            '</td>' +
+            '<td>' +
+            elem.jobTitle +
+            '</td>' +
+            '<td>' +
+            '<button class = "delete_user"> Удалить сотрудника</button>' +
+            '</td>'
 
         const deleteBtn = newField.querySelector('.delete_user')
-        deleteBtn.addEventListener('click' ,() => {
+        deleteBtn.addEventListener('click', () => {
             newField.parentNode.removeChild(newField)
             deleteBtn.setAttribute('id', index)
             workerArr.splice(index, 1)
@@ -151,19 +151,19 @@ const renderWorkerTable = () => {
 }
 
 const createNewEssense = (name, lastName, age, Birth, select, job) => {
-    if(select === 'front'){
-       const frontend = new FrontendDev(name, lastName, age, Birth)
+    if (select === 'front') {
+        const frontend = new FrontendDev(name, lastName, age, Birth)
         frontend.jobTitle = job
         workerArr.push(frontend)
-    } else if(select === 'back'){
-       const backEnd = new backendDev(name, lastName, age, Birth)
+    } else if (select === 'back') {
+        const backEnd = new backendDev(name, lastName, age, Birth)
         backEnd.jobTitle = job
         workerArr.push(backEnd)
-    } else if(select === 'design'){
-       const design = new designer(name, lastName, age, Birth)
+    } else if (select === 'design') {
+        const design = new designer(name, lastName, age, Birth)
         design.jobTitle = job
         workerArr.push(design)
-    } else if(select === 'analitick'){
+    } else if (select === 'analitick') {
         const analit = new googleAnalitick(name, lastName, age, Birth)
         analit.jobTitle = job
         workerArr.push(analit)
@@ -175,9 +175,9 @@ const createNewEssense = (name, lastName, age, Birth, select, job) => {
 const getDataUser = (e) => {
     let nameUse, lastName, age, Birth, select, selectJobTitle
 
-    if(nameInput.value === '' || lastNameInput.value === '' || ageInput.value === '' || dataBirthInput.value === ''){
+    if (nameInput.value === '' || lastNameInput.value === '' || ageInput.value === '' || dataBirthInput.value === '') {
         warnMessege.style.display = 'flex'
-    } else{
+    } else {
         nameUse = nameInput.value
         lastName = lastNameInput.value
         age = ageInput.value
@@ -191,10 +191,3 @@ const getDataUser = (e) => {
 btnSaveData.addEventListener('click', getDataUser)
 
 getDataStorage()
-
-
-
-
-
-
-
